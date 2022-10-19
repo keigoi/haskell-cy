9d454e4964537bc7896ee8d7bde17181fe822c7c
Keigo Imai <keigo.imai@gmail.com>
2022-10-19 15:09:14 +0900

    cy2 bekic??

--- Cy.hs
+++ Cy.hs
@@ -14,17 +14,17 @@ data Cy2_ f v w =
     | VarOt w
 
 newtype Cy2 f g =
-    MakeCy2 (forall v w. (v,w) -> (Cy2_ f v w, Cy2_ g w v))
+    MakeCy2 {extCy2::forall v w. ((v,w) -> Cy2_ f v w, (v,w) -> Cy2_ g w v)}
 
 data IListF x = Cons Int x deriving Show
 
 inf12 = MakeCy $ Cy (\v -> D $ Cons 1 (D $ Cons 2 $ Var v))
 
-inf1234 = MakeCy2 $ \(v,w) -> 
-    (
-        DD $ Cons 1 $ DD $ Cons 2 $ VarOt w, 
-        DD $ Cons 3 $ DD $ Cons 4 $ VarOt v
-    )
+-- inf1234 = MakeCy2 $ 
+--     (
+--         \(v,w) -> DD $ Cons 1 $ DD $ Cons 2 $ VarOt w, 
+--         \(v,w) -> DD $ Cons 3 $ DD $ Cons 4 $ VarOt v
+--     )
 
 class Fold (f :: * -> *) where
     data Cata f (g :: * -> *)
@@ -41,6 +41,21 @@ fold' _    (Var v)  = Var v
 
 fold :: Fold f1 => Cata f1 f2 -> Cy f1 -> Cy f2
 fold cata (MakeCy cy) = MakeCy (fold' cata cy)
+
+bekic :: Cy2 f g -> (Cy2_ f v (Cy2_ g w v), Cy2_ g w1 (Cy2_ f v2 (Cy2_ g w2 v2)))
+bekic cy2 =
+    (
+        Cy2 (\v -> 
+                let s = Cy2 (\w -> snd (extCy2 cy2) (v,w)) in
+                fst (extCy2 cy2) (v,s)
+            )
+        ,
+        Cy2 (\w ->
+                let s v = Cy2 (\w -> snd (extCy2 cy2) (v,w)) in
+                let t = Cy2 (\v -> fst (extCy2 cy2) (v,s v)) in
+                snd (extCy2 cy2) (t,w)
+            )
+    )
 
 inf34 = fold (ListC (\x r -> D $ Cons (x+1) r)) inf12
 
